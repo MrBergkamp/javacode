53406fd470c8babe9ac45bbfb097a23e71d38d86
MrBergkamp <40593973+MrBergkamp@users.noreply.github.com>
2018-06-26 09:47:26 +0000

    Update App.java

--- src/main/java/com/example/rest/App.java
+++ src/main/java/com/example/rest/App.java
@@ -51,7 +51,7 @@ public class App {
     private static void runGetRequest() {
  
     	// sample URL
-        String url = "http://140.86.15.104:3000/reactorcore/320/650/blue/MrBergkamp";
+        String url = "http://140.86.15.104:3000/reactorCore/320/650/blue/MrBergkamp";
         CloseableHttpResponse response = null;
  
         try {
